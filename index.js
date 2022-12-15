c73ab1dc4258f51e59ad062aaffc278063a6706e
Noah <business.noah@hotmail.com>
2022-12-15 00:45:23 -0700

    commit tree 956716bcf25fe31959bddffb0d7ab5a52c8e5763

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(data => {
+    renderBooks(data);
+    
+  })
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +21,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
